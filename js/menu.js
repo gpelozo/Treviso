d2b52363cc9c1944f7eb0e2fcd8cc2ca2c40ac78
cepoid <gpelozo@cepoid.com>
2022-05-20 04:45:37 -0300

    agregando animaciones, responsive reelaborado, mejora contraste texto, imagen polimeros aclarada

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,23 @@
-$(document).ready(function() {
-$(function() { 
-    $(".toggle").on("click", function() { 
-        if ($(".item").hasClass("active")) {
-            $(".item").removeClass("active");
-            $(this).find("a").html("<i class='fas fa-bars'></i>"); 
-        } 
-        else { 
-            $(".item").addClass("active");
-            $(this).find("a").html("<i class='fas fa-times'></i>");
-         } 
-        });
-     });
-})
+//Cuando el usuario haga click en el boton hamburguesa, permutara entre ocultar y mostrar el menu desplegable
+function myFunction(x) {
+  document.getElementById("myDropdown").classList.toggle("show");
+  x.classList.toggle("change");
+}
+
+//esto sirve para cerrar el menu si el usuario hace click fuera del menu desplegable
+window.onclick = function(event) {
+
+  if (!event.target.matches('.hamburguesa')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+      if (openDrowdown.class.List.contains('change')) {
+        openDrowdown.classList.remove('change');
+      }
+    }
+  }
+}
